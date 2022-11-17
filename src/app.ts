2f1c72ba05fbbccabb54a09324015900960b256d
Gabriel Martins <gabrielmartinsdesouzaa@hotmail.com>
2022-11-17 18:43:33 -0300

    Correção no nome da funcionalidade handlerErrors.ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import "express-async-errors";
 import express from "express";
 import { AppDataSource } from "./AppDataSource";
 import routes from "./routes";
-import { HandleErrors } from "./middlewares/HandleErrors";
+import { HandlerErrors } from "./middlewares/HandlerErrors";
 
 const PORT = process.env.PORT;
 
@@ -10,7 +10,7 @@ AppDataSource.initialize().then(() => {
   const app = express();
   app.use(express.json());
   app.use(routes);
-  app.use(HandleErrors);
+  app.use(HandlerErrors);
 
   return app.listen(PORT, () => {
     console.log(`Servidor iniciado em http://localhost:${PORT}`);
--- src/middlewares/HandlerErrors.ts
+++ src/middlewares/HandlerErrors.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-export const HandleErrors = (
+export const HandlerErrors = (
   error: Error,
   request: Request,
   respose: Response,
